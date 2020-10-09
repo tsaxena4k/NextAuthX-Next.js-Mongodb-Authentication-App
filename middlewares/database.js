4ccee796cb84f2faa3c57cae23f15892bd28f5d8
Tushar saxena <46051734+tsaxena4k@users.noreply.github.com>
2020-10-09 14:20:27 +0530

    Encoded URI

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(encodeURI(process.env.MONGODB_URI), {
+const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
