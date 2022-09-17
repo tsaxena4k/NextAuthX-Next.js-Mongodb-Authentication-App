26b4c99947e8edc3458f1575bdda5c70957184d1
Wajid <wajid@nejoumaljazeera.com>
2022-09-17 18:19:20 +0400

    Testing with website project libraries

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// Please do not use the array form (like ['tailwindcss', 'postcss-preset-env'])
+// it will create an unexpected error: Invalid PostCSS Plugin found: [0]
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  },
+};
