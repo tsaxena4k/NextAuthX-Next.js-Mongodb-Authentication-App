a64a1607c165ecb41616843a69cff232dcac1660
HaneenAlkhateeb <hne.ite@gmail.com>
2022-10-25 08:05:46 +0400

    ed

--- components/Login.jsx
+++ components/Login.jsx
@@ -2,15 +2,47 @@ import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { GoogleLogin } from 'react-google-login';
 import { useUser } from "../lib/hooks";
 import Link from 'next/link';
+//import { gapi } from 'gapi-script';
+
 
 export default function Login() {
     const router = useRouter();
     const [errorMsg, setErrorMsg] = useState("");
     const [user, { mutate }] = useUser();
     const [loading, isLoading] = useState(false);
+    
+    const responseGoogle = (err) => {
+        console.log(err);
+    }
+
+    const handleLogin = async googleData => {
+        const res = await fetch("/api/v1/auth/google", {
+        method: "POST",
+        body: JSON.stringify({
+        token: googleData.tokenId
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })  
+    if (res.status === 200) {
+        // store returned user somehow
+        const userObj = await res.json();
+        console.log('fsdafdsaf');
+        mutate(userObj);
+    }
+    
+  }
     useEffect(() => {
+       /**gapi.load("client:auth2", () => {
+            gapi.client.init({
+                clientId: "222480430426-hko2000qdbkc2isvpjdki7jhhqsjou7g.apps.googleusercontent.com",
+                scope: ''
+            });
+        });**/
         // redirect to home if user is authenticated
         if (user) router.replace("/");
     }, [user]);
@@ -51,34 +83,37 @@ export default function Login() {
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
                     <form className="space-y-6" onSubmit={onSubmit}>
-                    <div className="mt-4 space-y-4">
-                            <div className="flex items-center">
-                            <input
-                                id="push-everything"
-                                name="push-notifications"
-                                type="radio"
-                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                            />
-                            <label
-                             htmlFor="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
-                                Male
-                            </label>
+                    <div className="mt-4 space-y-4 ">
+                            <div className="flex justify-around">
+                                <div className="flex items-center">
+                                    <input
+                                        id="push-everything"
+                                        name="push-notifications"
+                                        type="radio"
+                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                    />
+                                    <label
+                                    htmlFor="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
+                                        Admin
+                                    </label>
+                                </div>
+                                <div className="flex items-center">
+                                    <input
+                                        id="push-email"
+                                        name="push-notifications"
+                                        type="radio"
+                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                    />
+                                    <label htmlFor="push-email" className="ml-3 block text-sm font-medium text-gray-700">
+                                        Poster
+                                    </label>
+                                </div>
                             </div>
-                            <div className="flex items-center">
-                            <input
-                                id="push-email"
-                                name="push-notifications"
-                                type="radio"
-                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                            />
-                            <label htmlFor="push-email" className="ml-3 block text-sm font-medium text-gray-700">
-                                Female
-                            </label>
-                            </div>
+                            
                         </div>
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                        Thesis name
+                        Email
                         </label>
                         <div className="mt-1">
                         <input
@@ -150,47 +185,24 @@ export default function Login() {
 
                     <div className="mt-6 grid grid-cols-3 gap-3">
                         <div>
-                        <a
-                            href="#"
-                            className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                        >
-                            <span className="sr-only">Sign in with Facebook</span>
-                            <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                            <path
-                                fillRule="evenodd"
-                                d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-                                clipRule="evenodd"
-                            />
-                            </svg>
-                        </a>
-                        </div>
-
-                        <div>
-                        <a
-                            href="#"
-                            className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                        >
-                            <span className="sr-only">Sign in with Twitter</span>
-                            <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                            </svg>
-                        </a>
-                        </div>
-
-                        <div>
-                        <a
-                            href="#"
-                            className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                        >
-                            <span className="sr-only">Sign in with GitHub</span>
-                            <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                            <path
-                                fillRule="evenodd"
-                                d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-                                clipRule="evenodd"
-                            />
-                            </svg>
-                        </a>
+                        <GoogleLogin
+                            className="inline-flex w-full justify-center rounded-md border border-gray-300
+                            bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                            clientId="222480430426-hko2000qdbkc2isvpjdki7jhhqsjou7g.apps.googleusercontent.com"
+                            buttonText="Login"
+                            render={renderProps => (
+                                    <button onClick={renderProps.onClick} disabled={renderProps.disabled}
+                                    className="inline-flex w-full justify-center rounded-md border 
+                                    border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                                >
+                                    <span className="sr-only">Sign in with Twitter</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16"> <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/> </svg> 
+                                </button>
+                              )}
+                            onSuccess={handleLogin}
+                            onFailure={responseGoogle}
+                            cookiePolicy={'single_host_origin'}
+                        />
                         </div>
                     </div>
                     </div>
