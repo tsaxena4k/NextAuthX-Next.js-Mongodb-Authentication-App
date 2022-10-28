83559d02bd1cc52d9da65c06f5dcf6d90a96231d
HaneenAlkhateeb <hne.ite@gmail.com>
2022-10-28 19:15:16 +0400

    edit

--- components/SearchProfile.jsx
+++ components/SearchProfile.jsx
@@ -6,41 +6,34 @@ import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import {
   Bars3Icon,
+  CalendarIcon,
+  CogIcon,
+  HomeIcon,
+  MagnifyingGlassCircleIcon,
+  MapIcon,
+  MegaphoneIcon,
+  SquaresPlusIcon,
+  UserGroupIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline';
 import { ChevronLeftIcon, EnvelopeIcon, FunnelIcon, MagnifyingGlassIcon, PhoneIcon } from '@heroicons/react/20/solid';
-import { Search } from '@material-ui/icons';
 
 const DashboardSection = () => {
     //const [user, { mutate }] = useCurrentUser();
     const [loading, isLoading] = useState(false);
-    const [universities, setData] = useState([]);
-    const [results, setResults] = useState([]);
-    const [selectedUniversity, setselectedUniversity] = useState('');
-    const yearRef = useRef();
-    const fnameRef = useRef();
-    const lnameRef = useRef();
-    const pagesRef = useRef();
-    const middleRef = useRef();
     const nameRef = useRef();
-    const alFnameRef = useRef();
-    const advisorRef = useRef();
-    const universityRef = useRef();
-    const thesisRef = useRef();
+    const bioRef = useRef();
+    const profilePictureRef = useRef();
     const [msg, setMsg] = useState({ message: '', isError: false });
     const router = useRouter();
     const [sidebarOpen, setSidebarOpen] = useState(false)
 
-
-    useEffect(() => {
-      isLoading(true)
-      fetch('/api/fetchUniversities')
-        .then((res) => res.json())
-        .then((data) => {
-          setData(data)
-          isLoading(false)
-      })
-  }, [])
+    const stats = [
+        { label: 'Founded', value: '2021' },
+        { label: 'Employees', value: '5' },
+        { label: 'Beta Users', value: '521' },
+        { label: 'Raised', value: '$25M' },
+    ]
     /**useEffect(() => {
         if (!user) {
             router.push('/');
@@ -50,34 +43,43 @@ const DashboardSection = () => {
         }
     }, [user]);**/
 
-    const searchRecords = async (event) => {
-      event.preventDefault();
-      isLoading(true);
-      const body = {
-          year: yearRef.current.value,
-          fname: fnameRef.current.value,
-          lname: lnameRef.current.value,
-          mname: middleRef.current.value,
-          university: universityRef.current.value,
-          dissertation_Title: thesisRef.current.value,
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        isLoading(true);
+        const formData = new FormData();
+
+        const body = {
+            email: emailRef.current.value,
+            fname: fnameRef.current.value,
+            lname: lnameRef.current.value,
+            university: universityRef.current.value,
+            createby: user.id,
+            createdate: new Date().toDateString()
+        };
+        const res = await fetch("/api/records", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        });
+        if (res.status === 200) {
+            const userData = await res.json();
+            mutate({
+                user: {
+                    ...user,
+                    ...userData.user,
+                },
+            });
+            setMsg({ message: 'Profile updated' });
+        } else {
+            setMsg({ message: await res.text(), isError: true });
+        }
+        isLoading(false);
+        setTimeout(() => setMsg(''), 2500);
       };
-      const res = await fetch(`/api/searchRecords`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(body)
-      });
-      if (res.status === 200) {
-          const userData = await res.json();
-          setResults(userData);
-          setMsg({ message: 'Added Successfully' });
-      } else {
-          setMsg({ message: await res.text(), isError: true });
-      }
-      isLoading(false);
-      setTimeout(() => setMsg(''), 2500);
-  };
 
     const user = {
+
+
         name: 'Tom Cook',
         imageUrl:
           'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -143,7 +145,7 @@ const DashboardSection = () => {
     return (
         <>
             <Head>
-                <title>Search</title>
+                <title>About Us</title>
             </Head>
             <div className="flex h-full">
         <Transition.Root show={sidebarOpen} as={Fragment}>
@@ -272,10 +274,23 @@ const DashboardSection = () => {
                       </div>
                       <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
                         <div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
-                          <h1 className="truncate text-2xl font-bold text-gray-900">Search Results</h1>
+                          <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
                         </div>
                         <div className="justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
-                          
+                          <button
+                            type="button"
+                            className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+                          >
+                            <EnvelopeIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                            <span>Message</span>
+                          </button>
+                          <button
+                            type="button"
+                            className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+                          >
+                            <PhoneIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                            <span>Call</span>
+                          </button>
                         </div>
                       </div>
                     </div>
@@ -285,27 +300,48 @@ const DashboardSection = () => {
                   </div>
                 </div>
 
-             
+                {/* Tabs */}
+
+                {/* Description list */}
+                <div className="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
+                  <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                    {Object.keys(profile.fields).map((field) => (
+                      <div key={field} className="sm:col-span-1">
+                        <dt className="text-sm font-medium text-gray-500">{field}</dt>
+                        <dd className="mt-1 text-sm text-gray-900">{profile.fields[field]}</dd>
+                      </div>
+                    ))}
+                    <div className="sm:col-span-2">
+                      <dt className="text-sm font-medium text-gray-500">About</dt>
+                      <dd
+                        className="mt-1 max-w-prose space-y-5 text-sm text-gray-900"
+                        dangerouslySetInnerHTML={{ __html: profile.about }}
+                      />
+                    </div>
+                  </dl>
+                </div>
 
                 {/* Team member list */}
                 <div className="mx-auto mt-8 max-w-5xl px-4 pb-12 sm:px-6 lg:px-8">
+                  <h2 className="text-sm font-medium text-gray-500">Team members</h2>
                   <div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
-                    {(results.length > 0)?results.map((person) => (
+                    {team.map((person) => (
                       <div
-                        key={person._id}
+                        key={person.handle}
                         className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-pink-500 focus-within:ring-offset-2 hover:border-gray-400"
                       >
-                        
+                        <div className="flex-shrink-0">
+                          <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
+                        </div>
                         <div className="min-w-0 flex-1">
                           <a href="#" className="focus:outline-none">
                             <span className="absolute inset-0" aria-hidden="true" />
-                            <p className="text-sm font-medium text-gray-900">{person.lname}, {person.fname} </p>
-                            <p className="truncate text-sm text-gray-500">{person.universityDetails[0].name}</p>
-                            <p className="truncate text-sm text-gray-500">{person.year}</p>
+                            <p className="text-sm font-medium text-gray-900">{person.name}</p>
+                            <p className="truncate text-sm text-gray-500">{person.role}</p>
                           </a>
                         </div>
                       </div>
-                    )):<div></div>}
+                    ))}
                   </div>
                 </div>
               </article>
@@ -314,30 +350,26 @@ const DashboardSection = () => {
               <div className="px-6 pt-6 pb-4">
                 <h2 className="text-lg font-medium text-gray-900">Directory</h2>
                 <p className="mt-1 text-sm text-gray-600">Search directory of 3,018 Records</p>
-                <form className="mt-6 flex space-x-4" onSubmit={searchRecords} action="#" method="POST">
+                <form className="mt-6 flex space-x-4" action="#">
                   <div className="min-w-0 flex-1">
                     <label htmlFor="search" className="sr-only">
                       Search
                     </label>
                     <div className="relative rounded-md shadow-sm">
                       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                        <button
-                              type="button"
-                              onClick={() => searchRecords()}>
-                        <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /></button>
+                        <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
                       </div>
                       <input
                         type="search"
                         name="search"
                         id="search"
-                        ref={thesisRef}
                         className="block w-full rounded-md border-gray-300 pl-10 focus:border-pink-500 focus:ring-pink-500 sm:text-sm"
-                        placeholder="Thesis Keyword"
+                        placeholder="Search"
                       />
                     </div>
                   </div>
                   <button
-                    onSubmit={() => searchRecords()}
+                    onSubmit={handleSubmit}
                     type="submit"
                     className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-3.5 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
                   >
@@ -353,28 +385,11 @@ const DashboardSection = () => {
                       <h3>First Name</h3>
                     </div>
                     <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        ref={fnameRef}
+                        type="search"
+                        name="search"
+                        id="search"
                         className="block w-full rounded-md border-gray-300 pl-10 focus:border-pink-500 focus:ring-pink-500 sm:text-sm"
-                        placeholder="First Name"
-                      />
-                  </div>
-
-                  <div className="relative">
-                    <div className="sticky top-0 z-10 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
-                      <h3>Middle Name</h3>
-                    </div>
-                    <input
-                        type="text"
-                        name="middle-name"
-                        id="middle-name"
-                        autoComplete="middle-name"
-                        ref={middleRef}
-                        className="block w-full rounded-md border-gray-300 pl-10 focus:border-pink-500 focus:ring-pink-500 sm:text-sm"
-                        placeholder="Middle Name"
+                        placeholder="Search"
                       />
                   </div>
 
@@ -383,12 +398,11 @@ const DashboardSection = () => {
                       <h3>Last Name</h3>
                     </div>
                     <input
-                        type="text"
-                        name="last-name"
-                        id="last-name"
-                        ref={lnameRef}
+                        type="search"
+                        name="search"
+                        id="search"
                         className="block w-full rounded-md border-gray-300 pl-10 focus:border-pink-500 focus:ring-pink-500 sm:text-sm"
-                        placeholder="Last Name"
+                        placeholder="Search"
                       />
                   </div>
 
@@ -396,35 +410,24 @@ const DashboardSection = () => {
                     <div className="sticky top-0 z-10 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
                       <h3>University Name</h3>
                     </div>
-                    <select
-                            id="country"
-                            name="country"
-                            autoComplete="country-name"
-                            ref={universityRef}
-                            value={selectedUniversity}
-                            onChange={setselectedUniversity}
-                            className="mt-1 block w-full rounded-md border border-gray-300 
-                            bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none 
-                            focus:ring-indigo-500 sm:text-sm"
-                            >
-                              <option value=""></option>
-                            {universities.map((item) => (
-                                    <option key={item.id} value={item._id}>{item.name}</option>
-                            ))}
-                      </select>
+                    <input
+                        type="search"
+                        name="search"
+                        id="search"
+                        className="block w-full rounded-md border-gray-300 pl-10 focus:border-pink-500 focus:ring-pink-500 sm:text-sm"
+                        placeholder="Search"
+                      />
                   </div>
-
                   <div className="relative">
                     <div className="sticky top-0 z-10 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
-                      <h3>Year of Degree</h3>
+                      <h3>Thesis Name</h3>
                     </div>
                     <input
-                        type="number"
-                        name="year"
-                        id="year"
-                        ref={yearRef}
+                        type="search"
+                        name="search"
+                        id="search"
                         className="block w-full rounded-md border-gray-300 pl-10 focus:border-pink-500 focus:ring-pink-500 sm:text-sm"
-                        placeholder="Year"
+                        placeholder="Search"
                       />
                   </div>
               </nav>
